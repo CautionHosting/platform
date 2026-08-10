9870316e316742e2a6961469f525363a00e40956
RyanSquared <ryan@distrust.co>
2026-08-10 08:27:49 -0400

    migration 42: if not exists email_verification_sent_at

--- src/api/migrations/044_email_verification_throttle.sql
+++ src/api/migrations/044_email_verification_throttle.sql
@@ -2,6 +2,6 @@
 -- SPDX-License-Identifier: AGPL-3.0-only OR LicenseRef-Commercial
 
 ALTER TABLE users
-    ADD COLUMN email_verification_sent_at TIMESTAMPTZ;
+ADD COLUMN IF NOT EXISTS email_verification_sent_at TIMESTAMPTZ;
 
 COMMENT ON COLUMN users.email_verification_sent_at IS 'Timestamp when the latest email verification message was sent';
